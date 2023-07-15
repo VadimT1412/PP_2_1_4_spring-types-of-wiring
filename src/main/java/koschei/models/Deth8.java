f770347d7e02aadd40df94cd16cc5396c66275db
Вадим <124818150+VadimT1412@users.noreply.github.com>
2023-07-16 00:32:26 +0500

    decided

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -4,5 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Deth8 {
+    private String string;
 
+    @Override
+    public String toString() {
+        return "";
+    }
 }
+
+
--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -9,7 +9,7 @@ public class Needle7 {
 
     @Override
     public String toString() {
-        return ", смерть Кощея на игле :(";
+        return ", смерть Кощея на игле :(" + deth.toString();
     }
 
     @Autowired
